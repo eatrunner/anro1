0833a09ec875e3e1c441755ead077047638b5e4b
korosmateusz <korosmateusz@protonmail.ch>
2016-04-12 15:34:11 +0200

    Dodano prototyp publishera

--- src/test_olek.cpp
+++ src/test_olek.cpp
@@ -37,6 +37,7 @@
 #include <ros/ros.h>
 #include <visualization_msgs/Marker.h>
 #include "std_msgs/String.h"
+#include <sstream>
 
 void initMarker(visualization_msgs::Marker& marker){
   marker.type = visualization_msgs::Marker::CUBE;
@@ -104,9 +105,12 @@ int main( int argc, char** argv )
   ros::init(argc, argv, "basic_shapes");
   ros::NodeHandle n;
   ros::Rate r(30);
+  /*Publishers*/
   ros::Publisher marker_pub = n.advertise<visualization_msgs::Marker>("visualization_marker", 1);
-  ros::Subscriber lights_infoSub = n.subscribe("lights_info", 1000, lights_infoCallback);
-  ros::Subscriber turns_infoSub = n.subscribe("turns_info", 1000, turns_infoCallback);
+  ros::Publisher car_info_pub = n.advertise<std_msgs::String>("car_info", 1000);
+  /*Subscribers*/
+  ros::Subscriber lights_info_pub = n.subscribe("lights_info", 1000, lights_infoCallback);
+  ros::Subscriber turns_info_pub = n.subscribe("turns_info", 1000, turns_infoCallback);
   visualization_msgs::Marker m1;
 
   m1.id = 1;
@@ -119,6 +123,13 @@ int main( int argc, char** argv )
   {
     moveMarker(m1,state_m1,move_m1);
     marker_pub.publish(m1);
+    
+    std_msgs::String car_info;
+    std::stringstream ss;
+    ss << "Car state: " << state;
+    car_info.data = ss.str();
+    car_info_pub.publish(car_info);
+    
     r.sleep();
   }
 }
