6f2a060e25b8995100bcd5530ee0dbaf579c2282
KacperSzczypior <ogonkici@gmail.com>
2016-04-14 19:51:36 +0200

    Update turns.cpp

--- src/turns.cpp
+++ src/turns.cpp
@@ -15,58 +15,57 @@ public:
 	bool west;
 	double x;
 	double y;
-    Turn(double ix, double iy,string type){
-	x=ix;
-	y=iy;
-	north=transform(type)[0];
-	west=transform(type)[1];
-}
-    bool* transform(string type)
-{
-    bool result[2];
-    if(type=="turningUpLeft")
-    {
-        result[0]=true;result[1]=true;
-    }
-    if(type=="turningUpRight")
-    {
-        result[0]=true;result[1]=false;
-    }
-    if(type=="turningDownLeft")
-    {
-        result[0]=false;result[1]=true;
-    }
-    if(type=="turningDownRight")
-    {
-        result[0]=false;result[1]=false;
-    }
-	return result;
-}
+	Turn(double ix, double iy, string type){
+		x = ix;
+		y = iy;
+		transform(type);
+	}
+	void transform(string type)
+	{
+		if (type == "turningUpLeft")
+		{
+			north = true; west = true;
+		}
+		if (type == "turningUpRight")
+		{
+			north = true; west = false;
+		}
+		if (type == "turningDownLeft")
+		{
+			north = false; west = true;
+		}
+		if (type == "turningDownRight")
+		{
+			north = false; west = false;
+		}
+		return result;
+	}
 };
 
 
 class Turns{
 public:
-void reset(){
-    Turn* c;
-    for(list<Turn*>::iterator it = turns.begin();it!=turns.end();it++){
-        delete *it;
+	Turns(){
+		
+	}
+	void reset(){
+		while (!turns->empty()){
+			turns->pop_front();
+		}
+	}
 
-}
-}
+	void addTurn(int x, int y, string type){
+		Turn turn(x, y, type);
+		turns->push_back(turn);
+	}
 
-void addTurn(int x, int y, string type){
-	Turn* ptr = new Turn(x,y,type);
-    turns.push_back(ptr);
-}
-
-list<Turn*> giveList()
-{
-    return turns;
-}
+	list<Turn> giveList()
+	{
+		return turns;
+	}
 
 private:
-list<Turn*> turns;
+	list<Turn> turns;
 
 };
 
@@ -75,69 +74,51 @@ bool ready = false;
 
 Turns turns;
 void process(const anro1::mapMessage::ConstPtr& msg){
-    if(msg->type!="fourLanes"){
-    turns.addTurn(msg->x,msg->y,msg->type);
-    ready = true;
-    	
-    }
+	if (msg->type != "fourLanes"){
+		turns.addTurn(msg->x, msg->y, msg->type);
+		ready = true;
+
+	}
 }
 int main(int argc, char **argv)
 {
-  
-  ros::init(argc, argv, "Lights");
 
- 
+	ros::init(argc, argv, "Lights");
 
-  ros::NodeHandle n;
-  ros::Publisher chatter_pub = n.advertise<anro1::turnsVector>("turns_info", 10);
-  ros::Subscriber sub = n.subscribe("map_info", 10, process);
-  ros::Rate loop_rate(10);
-  anro1::turn *turnmsg;
-  anro1::turnsVector* turnVector;
 
- 
-  
-  while (ros::ok())
-  {
-    ros::spinOnce();
-	if(!ready)
-		continue;
-turnVector = new anro1::turnsVector;
-  turnVector->size=0;
-     for(list<Turn*>::iterator it = turns.giveList().begin();it!=turns.giveList().end();it++)
-{
-        turnmsg=new anro1::turn;
-        turnmsg->x=(*it)->x;
-        turnmsg->y=(*it)->y;
-        turnmsg->NS=(*it)->north;
-        turnmsg->WE=(*it)->west;
-        turnVector->turns.push_back(*turnmsg);
-        turnVector->size++;
+
+	ros::NodeHandle n;
+	ros::Publisher chatter_pub = n.advertise<anro1::turnsVector>("turns_info", 10);
+	ros::Subscriber sub = n.subscribe("map_info", 10, process);
+	ros::Rate loop_rate(10);
+	//anro1::turn turnmsg;
+	//anro1::turnsVector turnVector;
+
+
+
+	while (ros::ok())
+	{
+		ros::spinOnce();
+		
+		anro1::turnsVector turnVector;
+		turnVector.size = 0;
+		for (list<Turn*>::iterator it = turns.giveList().begin(); it != turns.giveList().end(); it++)
+		{
+			anro1::turn turnmsg;
+			turnms.>x = (*it)->x;
+			turnmsg.y = (*it)->y;
+			turnmsg.NS = (*it)->north;
+			turnmsg.WE = (*it)->west;
+			turnVector.turns.push_back(turnmsg);
+			turnVector.size++;
+		}
+
+		chatter_pub.publish(turnVector);
+
+		
+		loop_rate.sleep();
+
+	}
+
+	return 0;
 }
-	
-    chatter_pub.publish(*turnVector);
-
-    delete &(turnVector->turns);
-    delete turnVector;
-    loop_rate.sleep();
-
-}
-
-  return 0;
-}
-
-  
-
-  
-  
-
- 
- 
- 
-
-  
-
- 
-
-  
-
