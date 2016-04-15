3cdc18c8bee82cc602a6dd713f7447deb103e9e8
KacperSzczypior <ogonkici@gmail.com>
2016-04-15 10:47:46 +0200

    Update lights.cpp

--- src/lights.cpp
+++ src/lights.cpp
@@ -7,128 +7,117 @@
 #include <list>
 using namespace std;
 
-class CrossRoad{
-public:
-	CrossRoad(double ix, double iy) :
-	north(true),
-	west(true),
-	south(true),
-	east(true),
-	timeToChange(1000),
-	northgreen(true),
-	x(ix),
-	y(iy),
-	time(0){
-	}	
 
-	bool north, west, south, east;
-	bool northgreen;
-	int time;
-	int timeToChange;
-	double x, y;
-	void incrementTime(){
-	time++;
-	if(time>timeToChange){
-		northgreen=!northgreen;
-		time=0;
-	}
-}
+class Crossroad{
+public:
+    Crossroad(double ix, double iy) :
+        north(true),
+        west(true),
+        south(true),
+        east(true),
+        timeToChange(1000),
+        northgreen(true),
+        x(ix),
+        y(iy),
+        time(0){
+        }
+
+        bool north, west, south, east;
+        bool northgreen;
+        int time;
+        int timeToChange;
+        double x, y;
+        void incrementTime(){
+        time++;
+        if(time>timeToChange){
+            northgreen=!northgreen;
+            time=0;
+        }
+    }
 };
 
-class CrossRoads
-{
+
+class Crossroads{
 public:
- void reset(){
-	CrossRoad* c;
-    for(list<CrossRoad*>::iterator it = crossRoads.begin();it!=crossRoads.end();it++){
-		delete *it;
-		
-}
-    	 
-   }
- 	 
+    Crossroads(){
+        crossroads = new list<Crossroad>;
+    }
+    void reset(){
+        while (!crossroads->empty()){
+            crossroads->pop_front();
+        }
+    }
 
-void tick(){
-    for(list<CrossRoad*>::iterator it = crossRoads.begin();it!=crossRoads.end();it++){
-		(*it)->incrementTime();
-}
-}
-void addCrossRoad(double x, double y){
-	CrossRoad* ptr = new CrossRoad(x,y);
-    crossRoads.push_back(ptr);
-}
+    void addCrossroad(int x, int y, string type){
+        Crossroad crossroad(x, y);
+        crossroads->push_front(crossroad);
+    }
+    void tick(){
+        for (list<Crossroad>::iterator it = giveList()->begin(); it != giveList()->end(); it++){
+            (*it).incrementTime();
+        }
+    }
 
-list<CrossRoad*> giveList()
-{
-    return crossRoads;
-}
+    list<Crossroad>* giveList()
+    {
+        return crossroads;
+    }
 
 private:
-list<CrossRoad*> crossRoads;
+    list<Crossroad>* crossroads;
 
 };
- 
- 
-  
 
-CrossRoads crossRoads;
+bool ready = false;
+
+
+Crossroads crossroads;
 void process(const anro1::mapMessage::ConstPtr& msg){
-    if(msg->type=="fourLanes")
-        crossRoads.addCrossRoad(msg->x,msg->y);
+    ROS_INFO("Ja slyszu");
+    if (msg->type == "fourLanes"){
+        crossroads.addCrossroad(msg->x, msg->y, msg->type);
+        ready = true;
+    }
 }
 int main(int argc, char **argv)
 {
-  
-  ros::init(argc, argv, "Lights");
 
- 
+    ros::init(argc, argv, "Lights");
 
-  ros::NodeHandle n;
-  ros::Publisher chatter_pub = n.advertise<anro1::lightsVector>("lights_info", 10);
-  ros::Subscriber sub = n.subscribe("map_info", 10, process);
-  ros::Rate loop_rate(10);
-  anro1::light *lightmsg;
-  anro1::lightsVector* lightVector;
 
- 
-  
-  while (ros::ok())
-  {
-    ros::spinOnce();
-    
-	
-lightVector = new anro1::lightsVector;
-  lightVector->size=0;
-     for(list<CrossRoad*>::iterator it = crossRoads.giveList().begin();it!=crossRoads.giveList().end();it++)
-{
-        lightmsg=new anro1::light;
-        lightmsg->x=(*it)->x;
-        lightmsg->y=(*it)->y;
-        lightmsg->NS=(*it)->northgreen;
-        lightmsg->WE=!(*it)->northgreen;
-        lightVector->lights.push_back(*lightmsg);
-        lightVector->size++;
+
+    ros::NodeHandle n;
+    ros::Publisher chatter_pub = n.advertise<anro1::lightsVector>("lights_info", 10);
+    ros::Subscriber sub = n.subscribe("map_info", 20, process);
+    ros::Rate loop_rate(10);
+
+
+    while (ros::ok())
+    {
+        ros::spinOnce();
+        if(!ready){
+            continue;
+        }
+        anro1::lightsVector lightVector;
+       lightVector.size = 0;
+
+        for (list<Crossroad>::iterator it = crossroads.giveList()->begin(); it != crossroads.giveList()->end(); it++)
+        {
+            anro1::light lightmsg;
+            lightmsg.x = (*it).x;
+            lightmsg.y = (*it).y;
+            lightmsg.NS = (*it).northgreen;
+            lightmsg.WE = !(*it).northgreen;
+            lightVector.lights.push_back(lightmsg);
+            lightVector.size++;
+        }
+
+        chatter_pub.publish(lightVector);
+
+
+        loop_rate.sleep();
+
+    }
+
+    return 0;
 }
-	
-    
-
-
-
-   
-    chatter_pub.publish(*lightVector);
-
-    delete &(lightVector->lights);
-
-
-
-crossRoads.tick();
-    loop_rate.sleep();
-  }
-
-
-  return 0;
-}
-
-
-
-
