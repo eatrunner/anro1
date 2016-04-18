9e56492c2bbe530b591410519886df547d3e6fa2
KacperSzczypior <ogonkici@gmail.com>
2016-04-18 11:52:42 +0200

    Update lights.cpp

--- src/lights.cpp
+++ src/lights.cpp
@@ -3,76 +3,103 @@
 #include "anro1/light.h"
 #include "anro1/lightsVector.h"
 #include "anro1/mapMessage.h"
+#include <stdlib.h>
+#include <ctime>
 #include <sstream>
 #include <list>
-#include <ctime>
 using namespace std;
 
 
+
+//reprezentacja pojedynczego skrzyzowania
 class Crossroad{
 public:
-	Crossroad(double ix, double iy) :
-		north(true),
-		west(true),
-		south(true),
-		east(true),
-		timeToChange(5),
-		northgreen(true),
-		x(ix),
-		y(iy)
-		{
-			time = new int;
-			*time = 0;
+    Crossroad(double ix, double iy) :
+        north(true),
+        west(true),
+        south(true),
+        east(true),
+        northGreen(true),
+        x(ix),
+        y(iy),
+        bothRed(false),
+        westGreen(false),
+        greenTime(5),
+        nextNorth(false),
+        redTime(0),
+        time(0){
+          timeToChange=rand()%4+greenTime;//czas w sekundach miedzy zmiana swiatel
+    }
 
-	}
+    bool north, west, south, east;//drogi wylotowe z skrzyzowania
+    bool northGreen;
+    bool westGreen;
+    bool bothRed;
+    bool nextNorth;
+    int time;
+    int timeToChange;
+    int greenTime;
+    int redTime;
+    double x, y;
+    void incrementTime(){
+        time++;
+        if(time>timeToChange){
+            time=0;
+            if(bothRed){
+               if(nextNorth){
+                   northGreen=true;
+                   westGreen=false;
+                   nextNorth=false;
+               }
+               else{
+                   northGreen=false;
+                   westGreen=true;
+                   nextNorth=true;
+               }
+              timeToChange=greenTime;
+               bothRed=false;
+            }
+            else{
+                northGreen=false;
+                westGreen=false;
+                bothRed=true;
+                timeToChange=redTime;
 
-	bool north, west, south, east;
-	bool northgreen;
-	int* time;
-	int timeToChange;
-	double x, y;
-	void incrementTime(){
-		int& ref = *time;
-		ref++;
-		if (ref>timeToChange){
-			northgreen = !northgreen;
-			ref = 0;
-		}
-	}
-	~Crossroad(){
-		delete time;
-	}
+            }
+
+        }
+    }
 };
 
-
+//kontener skrzyzowan
 class Crossroads{
 public:
-	Crossroads(){
-		crossroads = new list<Crossroad>;
-	}
-	void reset(){
-		while (!crossroads->empty()){
-			crossroads->pop_front();
-		}
-	}
+    Crossroads(){
+        crossroads = new list<Crossroad>;
+    }
+    void reset(){
+        while (!crossroads->empty()){
+            crossroads->pop_front();
+        }
+    }
 
-	void addCrossroad(int x, int y, string type){
-		Crossroad crossroad(x, y);
-		crossroads->push_front(crossroad);
-	}
-	void tick(){
-		for (list<Crossroad>::iterator it = giveList()->begin(); it != giveList()->end(); it++){
-			(*it).incrementTime();
-		}
-	}
+    void addCrossroad(int x, int y, string type){
+        Crossroad crossroad(x, y);
+        crossroads->push_front(crossroad);
+    }
+    void tick(){//inkrementacja czasu na skrzyzowaniach
+        for (list<Crossroad>::iterator it = giveList()->begin(); it != giveList()->end(); it++){
+            (*it).incrementTime();
+        }
+    }
 
-	list<Crossroad>* giveList()
-	{
-		return crossroads;
-	}
+    list<Crossroad>* giveList()
+    {
+        return crossroads;
+    }
 
 private:
-	list<Crossroad>* crossroads;
+    list<Crossroad>* crossroads;
 
 };
 
@@ -81,58 +108,54 @@ bool ready = false;
 
 Crossroads crossroads;
 void process(const anro1::mapMessage::ConstPtr& msg){
-	ROS_INFO("Ja slyszu");
-	if (msg->type == "fourLanes"){
-		crossroads.addCrossroad(msg->x, msg->y, msg->type);
-		ready = true;
-	}
+    ROS_INFO("Ja slyszu");
+    if (msg->type == "fourLanes"){
+        crossroads.addCrossroad(msg->x, msg->y, msg->type);
+        ready = true;
+    }
 }
 int main(int argc, char **argv)
 {
 
-	ros::init(argc, argv, "Lights");
+    ros::init(argc, argv, "Lights");
+    srand(time(NULL));
+    ros::NodeHandle n;
+    ros::Publisher chatter_pub = n.advertise<anro1::lightsVector>("lights_info", 10);//utworzenie kanalu do nadawania
+    ros::Subscriber sub = n.subscribe("map_info", 20, process);//subskrypcja kanalu z informacjami
+    ros::Rate loop_rate(10);
+    time_t lasttick, thistick;
+    time(&lasttick);
+    while (ros::ok())
+    {
+        ros::spinOnce();
+        if(!ready){
+            continue;
+        }
+        time(&thistick);
+        if (lasttick + 1 < thistick){//uplynela conajmniej sekunda od ostatniej inkrementacji
+                    lasttick=lasttick+1;
+                    crossroads.tick();
+                    }
+        anro1::lightsVector lightVector;
+        lightVector.size = 0;
+
+        for (list<Crossroad>::iterator it = crossroads.giveList()->begin(); it != crossroads.giveList()->end(); it++)//dodajemy wiadomosci do wektora
+        {
+            anro1::light lightmsg;
+            lightmsg.x = (*it).x;
+            lightmsg.y = (*it).y;
+            lightmsg.NS = (*it).northGreen;
+            lightmsg.WE = (*it).westGreen;
+            lightVector.lights.push_back(lightmsg);
+            lightVector.size++;
+        }
+
+        chatter_pub.publish(lightVector);
 
 
+        loop_rate.sleep();
 
-	ros::NodeHandle n;
-	ros::Publisher chatter_pub = n.advertise<anro1::lightsVector>("lights_info", 10);
-	ros::Subscriber sub = n.subscribe("map_info", 20, process);
-	ros::Rate loop_rate(10);
-	anro1::light lightmsg;
-	anro1::lightsVector lightVector;
+    }
 
-	time_t lasttick = time(nullptr);
-
-	while (ros::ok())
-	{
-		ros::spinOnce();
-		if (!ready){
-			continue;
-		}
-		anro1::lightsVector lightVector;
-		lightVector.size = 0;
-		if (lasttick + 1 < time(nullptr)){
-			lasttick++;
-			crossroads.tick();
-		}
-
-		for (list<Crossroad>::iterator it = crossroads.giveList()->begin(); it != crossroads.giveList()->end(); it++)
-		{
-			anro1::light lightmsg;
-			lightmsg.x = (*it).x;
-			lightmsg.y = (*it).y;
-			lightmsg.NS = (*it).northgreen;
-			lightmsg.WE = !(*it).northgreen;
-			lightVector.lights.push_back(lightmsg);
-			lightVector.size++;
-		}
-
-		chatter_pub.publish(lightVector);
-
-
-		loop_rate.sleep();
-
-	}
-
-	return 0;
+    return 0;
 }
