12e79d715a4aa2f103e3c5461b6604dccf960d36
KacperSzczypior <ogonkici@gmail.com>
2016-04-21 19:34:41 +0200

    Update future.cpp

--- src/future.cpp
+++ src/future.cpp
@@ -3,6 +3,7 @@
 #include "anro1/light.h"
 #include "anro1/lightsVector.h"
 #include "anro1/mapMessage.h"*/
+#include <iostream>
 #include <stdlib.h>
 #include <vector>
 #include <ctime>
@@ -11,29 +12,31 @@
 #include <queue>
 using namespace std;
 
-class Out{
+class Point{
 public:
 	int x, y;
-	Out(int ix, int iy){
-		x = ix;
-		y = iy;
+	Point(int ix, int iy) : x(ix), y(iy){}
+	Point(const Point& p) : x(p.x), y(p.y){}
+	Point() : x(0), y(0){}
+	bool operator<(const Point& second){
+		if (x = second.x)
+			return y < second.y;
+		else return x < second.x;
+	}
+	bool operator==(const Point& second){
+		return x == second.x&&y == second.y;
 	}
 };
 
 
-class Entry{
+
+
+class Entry : public Point{
 public:
-	int x, y;
+	Entry(int ix, int iy) : Point(x,y),light(false){}
+	vector<Point> left,straight,right;
 	bool light;
-	Entry(int ix, int iy) : 
-		light(false)
-	{
-		x = ix;
-		y = iy;
-	}
-	vector<Out> left,straight,right;
 };
-
 
 
 class Side{
@@ -41,100 +44,173 @@ public:
 	vector<Entry> entries;
 };
 
-class Point{
-public:
-	int x, y;
-};
+
 
 class InSide{
 public:
 	vector<Point> ins, outs;
 };
+vector<InSide> createPoints(int n, int w, int e, int s){
+	vector<InSide> sides(4);
+	Point p;
+	p.x = 0;
+	p.y = 0;
+	for (int i = 0; i < s; i++){
+		sides[0].ins.push_back(p);
+		sides[0].outs.push_back(p);
+	}
+	for (int i = 0; i < e; i++){
+		sides[1].ins.push_back(p);
+		sides[1].outs.push_back(p);
+	}for (int i = 0; i < w; i++){
+		sides[2].ins.push_back(p);
+		sides[2].outs.push_back(p);
+	}for (int i = 0; i < n; i++){
+		sides[3].ins.push_back(p);
+		sides[3].outs.push_back(p);
+	}
+	return sides;
+}
 
-class Crossroad{
-public:
+class Light{
 	Side sides[4];
-	/*Crossroad(InSide insides[4]){//kazdy kolejny po prawej
-		for (int i = 0; i < 4; i++){//ins i outs od srodka pasa
-			int n = insides[i].ins.size();
-			for (int k = 0; k < n; k++){//inicjalizacja wjazdow
-				Point p = insides[i].ins[k];
-				sides[i].entries.push_back(Entry(p.x, p.y));
-			}
-		}
-		for (int i = 0; i < 4; i++){//make straights
-			int k = i + 2;
-			k = k % 4;
-			int m = insides[k].outs.size();
-			int n = sides[i].entries.size();
-			for (int l= 0; l < n&&l<m; l++){
-				Point p = insides[k].outs[l];
-				sides[i].entries[l].straight.push_back(Out(p.x,p.y));
-			}
-		}
-
-		for (int i = 0; i < 4; i++){//make rights
-			int k = i + 1;
-			k = k % 4;
-			int z = sides[i].entries.size();
-			int m = insides[k].outs.size();
-			int n = sides[i].entries.size();
-			Point p = insides[k].outs[m-1];
-			sides[i].entries[z-1].right.push_back(Out(p.x, p.y));
-		}
-
-		for (int i = 0; i < 4; i++){//lefts
-			int k = i + 3;
-			k = k % 4;
-			
-			int m = insides[k].outs.size();
-			int n = sides[i].entries.size();
-			for (int j = 0; j < insides[k].outs.size - 1; j++){
-				Point p = insides[k].outs[j];
-				sides[i].entries[0].left.push_back(Out(p.x, p.y));
-			}
-		}
-		*/
-	Crossroad(InSide insides[4]) : 
-		nsTogether(false),
-		weTogether(false)
-		{//kazdy kolejny po prawej   0 na poludniu
+	Light(vector<InSide> insides){
 		for (int i = 0; i < 4; i++){//ins i outs od srodka pasa
 			int insSize = insides[i].ins.size();
 			for (int k = 0; k < insSize; k++){//inicjalizacja wjazdow
 				Point p = insides[i].ins[k];
 				sides[i].entries.push_back(Entry(p.x, p.y));
+				//cout << "pushentry" << endl;
 			}
 		}
+	}
+
+};
+
+enum LightState{
+	allgreen,allred
+};
+
+class Crossroad{
+public:
+	Side sides[4];
+	void addinfo(list<Route*>* routes){
+		for (int i = 0; i < 4; i++){
+			for (int j = 0; j < sides[i].entries.size(); j++){
+				for (list<Route*>::iterator it = routes->begin(); ; it++){//dopisz kierunki
+					if (it == routes->end()){
+						cout << "Route with entry" << sides[i].entries[j].x << "," << sides[i].entries[j].y << "not found!" << endl;
+					}
+					if ((*it)->end == sides[i].entries[j]){
+						if (sides[i].entries[j].left.size() != 0){
+							(*it)->left = true;
+						}
+						if (sides[i].entries[j].straight.size() != 0){
+							(*it)->straight = true;
+						}
+						if (sides[i].entries[j].right.size() != 0){
+							(*it)->right = true;
+						}
+						break;
+					}
+				}
+				for (int k = 0; k < sides[i].entries[j].left.size(); k++){
+					for (list<Route*>::iterator it = routes->begin();; it++){
+						if (it == routes->end()){
+							cout << "Route with end" << sides[i].entries[j].left[k].x << "," << sides[i].entries[j].left[k].y << "not found!" << endl;
+						}
+						if ((*it)->begin == sides[i].entries[j].left[k]){
+							if (k > 0){
+								(*it)->leftRoute = true;
+							}
+							if (k < sides[i].entries[j].left.size() - 1){
+								(*it)->rightRoute = true;
+							}
+						}
+						break;
+					}
+				}
+				for (int k = 0; k < sides[i].entries[j].straight.size(); k++){
+					for (list<Route*>::iterator it = routes->begin();; it++){
+						if (it == routes->end()){
+							cout << "Route with end" << sides[i].entries[j].straight[k].x << "," << sides[i].entries[j].straight[k].y << "not found!" << endl;
+						}
+						if ((*it)->begin == sides[i].entries[j].straight[k]){
+							if (k > 0){
+								(*it)->leftRoute = true;
+							}
+							if (k < sides[i].entries[j].straight.size() - 1){
+								(*it)->rightRoute = true;
+							}
+						}
+					}
+				}
+				for (int k = 0; k < sides[i].entries[j].right.size(); k++){
+					for (list<Route*>::iterator it = routes->begin();; it++){
+						if (it == routes->end()){
+							cout << "Route with end" << sides[i].entries[j].right[k].x << "," << sides[i].entries[j].right[k].y << "not found!" << endl;
+						}
+						if ((*it)->begin == sides[i].entries[j].right[k]){
+							if (k > 0){
+								(*it)->leftRoute = true;
+							}
+							if (k < sides[i].entries[j].right.size() - 1){
+								(*it)->rightRoute = true;
+							}
+						}
+					}
+				}
+
+			}
+		}
+	}
+	void createAccessPoints(vector<InSide> insides){
+		for (int i = 0; i < 4; i++){//ins i outs od srodka pasa
+			int insSize = insides[i].ins.size();
+			for (int k = 0; k < insSize; k++){//inicjalizacja wjazdow
+				Point p = insides[i].ins[k];
+				sides[i].entries.push_back(Entry(p.x, p.y));
+				//cout << "pushentry" << endl;
+			}
+		}
+	}
+
+	void createStraightConnections(vector<InSide> insides){
 		for (int i = 0; i < 4; i++){//make straights
 			int opposideIndex = i + 2;
-			opposideIndex %=4;
+			opposideIndex %= 4;
 			int outsSize = insides[opposideIndex].outs.size();
 			int entriesSize = sides[i].entries.size();
 			for (int l = 0; l < entriesSize && l<outsSize; l++){
 				Point p = insides[opposideIndex].outs[l];
-				sides[i].entries[l].straight.push_back(Out(p.x, p.y));
+				sides[i].entries[l].straight.push_back(p);
+				//cout << "pushstraight" << endl;
 			}
 		}
+	}
 
-		for (int i = 0; i < 4; i++){//make 
+	void createRightConnections(vector<InSide> insides){
+		for (int i = 0; i < 4; i++){//prawo
 			int rightIndex = i + 1;
 			rightIndex %= 4;
 			int rightLane = sides[i].entries.size() - 1;
-			
+
 			int outsSize = insides[rightIndex].outs.size();//jesli naprzeciw 1 pas to break
-			int breakEarlier = 0;;
+			int breakEarlier = 0;
 			int opposideIndex = i + 2;
 			opposideIndex %= 4;
-			if (insides[opposideIndex].outs.size == 1 && insides[i].ins.size() == 1){
-				breakEarlier = outsSize-1;
+			if (insides[opposideIndex].outs.size() == 1 && insides[i].ins.size() == 1){//jesli naprzeciw 1 pas to break
+				breakEarlier = outsSize - 1;
 			}
-			for (int j = outsSize-1; j >= 0 + breakEarlier; j++){
+			for (int j = outsSize - 1; j >= 0 + breakEarlier; j--){
 				Point p = insides[rightIndex].outs[j];
-				sides[i].entries[rightLane].right.push_back(Out(p.x, p.y));
+				sides[i].entries[rightLane].right.push_back(p);
+				//cout << "pushright" << endl;
 			}
 		}
+	}
 
+	void createLeftConnections(vector<InSide> insides){
 		for (int i = 0; i < 4; i++){//lefts wszystkie!!
 			int leftIndex = i + 3;
 			leftIndex %= 4;
@@ -142,51 +218,147 @@ public:
 			int outsSize = insides[leftIndex].outs.size();
 			int opposideIndex = i + 2;
 			opposideIndex %= 4;
-			if (insides[opposideIndex].outs.size == 1 && insides[i].ins.size()==1){
+			if (insides[opposideIndex].outs.size() == 1 && insides[i].ins.size() == 1){//jesli naprzeciw 1 pas to break
 				outsSize--;
-				if (i % 2 == 0){
-					nsTogether = true;
-				}
-				else{
-					weTogether = true;
-				}
 			}
 			for (int j = 0; j < outsSize; j++){//jesli naprzeciw 1 pas to break
 				Point p = insides[leftIndex].outs[j];
-				sides[i].entries[leftLane].left.push_back(Out(p.x, p.y));
+				sides[i].entries[leftLane].left.push_back(p);
 			}
 		}
+	}
+
+	void createRemainingRightConnections(vector<InSide> insides){
 		for (int i = 0; i < 4; i++){
 			int rightIndex = i + 1;
 			rightIndex %= 4;
-			for (int k = sides[i].entries.size(); k >= 0; k--){
+			for (int k = sides[i].entries.size()-1; k >= 0; k--){
 				Entry* thisentry = &(sides[i].entries[k]);
 				if (thisentry->left.size() == 0 && thisentry->straight.size() == 0 && thisentry->right.size() == 0){
 					Point p;
 					if (insides[rightIndex].outs.size() >= k){
-						 p = insides[rightIndex].outs[k];
+						p = insides[rightIndex].outs[k-1];
 					}
 					else{
 						p = insides[rightIndex].outs[insides[rightIndex].outs.size() - 1];
 					}
-					thisentry->left.push_back(Out(p.x, p.y));
+					thisentry->left.push_back(p);
 				}
 			}
 		}
+	}
+	Crossroad(vector<InSide> insides) : 
+		nsTogether(false), redtime(2), greentime(6),
+		weTogether(false),
+		state(0),
+		side(0),
+		time(0)
+		{//kazdy kolejny po prawej   0 na poludniu
+			if (insides.size() != 4){
+				cout << "DISASTER";
+			}
 
+			if (insides[0].ins.size() == 1 && insides[2].ins.size() == 1){
+				nsTogether = true;
+			}
+			if (insides[1].ins.size() == 1 && insides[3].ins.size() == 1){
+				weTogether = true;
+			}
 
+			createAccessPoints(insides);
+			createStraightConnections(insides);
+			createRightConnections(insides);
+			createLeftConnections(insides);
+			createRemainingRightConnections(insides);
 	}
 	bool nsTogether, weTogether;
-
-	void changelights(){
+	int time,timeToChange;
+	int side,state;
+	int greentime, redtime;
+	vector<LightState> lightstate;
+	void setLightsStates(){
+		lightstate.push_back(allred);
+		lightstate.push_back(allgreen);
+	}
+	void goToState(LightState state, int side){
+		switch (state){
+		case allred:
+			for (int i = 0; i < sides[side].entries.size(); i++){
+				sides[side].entries[i].light = false;
+			}
+			timeToChange = 2;
+			break;
+		case allgreen:
+			for (int i = 0; i < sides[side].entries.size(); i++){
+				sides[side].entries[i].light = true;
+			}
+			timeToChange = 6;
+		}
 
 	}
 
+	bool test(bool show=false){
+		if (show)
+		for (int i = 0; i < 4; i++){
+			cout << sides[i].entries.size()<<"ent"<<endl;
+		}
+		if (show)
+		for (int i = 0; i < 4; i++){
+			for (int j = 0; j < sides[i].entries.size(); j++){
+				cout << sides[i].entries[j].left.size()<<"lewo"<<endl;
+				cout << sides[i].entries[j].straight.size()<<"prosto"<<endl;
+				cout << sides[i].entries[j].right.size() << "prawo" << endl;
+
+			}
+			cout << endl;
+		}
+		for (int i = 0; i < 4; i++){
+			for (int j = 0; j < sides[i].entries.size(); j++){
+				bool ok = false;
+				if (sides[i].entries[j].left.size() != 0){
+					ok = true;
+				}
+				if (sides[i].entries[j].straight.size() != 0)
+					ok = true;
+				if (sides[i].entries[j].right.size() != 0)
+					ok = true;
+				if (!ok){
+					return false;
+				}
+			}
+		}
+		return true;
+	}
+
+	void changelights(){
+		goToState(lightstate[state], side);
+		state++;
+		if (state>lightstate.size()){
+			state = 0;
+			side++;
+			side = side % 4;
+		}
+	}
+	void incrementTime(){
+		time++;
+		if (time > timeToChange){
+			time = 0;
+			changelights();
+		}
+	}
 };
-/*
-//kontener skrzyzowan
+
+
+
 class Crossroads{
 public:
+	void createRoutes(vector<Route>* routesToCheck){
+		for (int i = 0; i < crossroads->size(); i++){
+
+		}
+	}
+
+
 	Crossroads(){
 		crossroads = new list<Crossroad>;
 	}
@@ -196,8 +368,8 @@ public:
 		}
 	}
 
-	void addCrossroad(int x, int y, string type){
-		Crossroad crossroad(x, y, type);
+	void addCrossroad(vector<InSide> vec){
+		Crossroad crossroad(vec);
 		crossroads->push_front(crossroad);
 	}
 	void tick(){//inkrementacja czasu na skrzyzowaniach
@@ -216,10 +388,90 @@ private:
 
 };
 
-bool ready = false;
-
-
 Crossroads crossroads;
+
+bool testAllPossibilities(int size){
+	bool ok;
+	for (int i = 1; i <= size; i++){
+		for (int k = 1; k <= size; k++){
+			for (int m = 1; m <= size; m++){
+				for (int n = 1; n <= size; n++){
+					vector<InSide> insides = createPoints(i, k, m, n);
+					Crossroad c(insides);
+					ok = c.test();
+					if (!ok)
+						cout << i << " " << k << " " << m << " " << n << endl;
+					/*if (c.nsTogether || c.weTogether){
+						cout << i << " " << k << " " << m << " " << n << endl;
+					}*/
+				}
+			}
+		}
+	}
+	return ok;
+}
+
+class Route{
+public:
+	Point begin;
+	Point end;
+	bool straight;
+	bool left;
+	bool right;
+	bool leftRoute;
+	bool rightRoute;
+	Route(){
+		straight = false;
+		left = false;
+		right = false;
+		leftRoute = false;
+		rightRoute = false;
+	}
+	bool operator<(const Route& second){
+		if (begin == second.begin)
+			return end < second.end;
+		return begin < second.begin;
+	}
+};
+
+
+
+
+
+
+
+
+
+int main(){
+
+}
+
+
+
+
+
+/*int main(){
+	//return 0;
+	vector<InSide> insides = createPoints(1, 1, 1, 1);
+	Crossroad c(insides);
+	//cout << c.test(true);
+	int x;
+	//cin >> x;
+	bool ok = true;
+	int counter = 0;
+	int size = 3;
+	ok = testAllPossibilities(size);
+	cout << ok;
+	cin >> x;
+
+
+
+
+
+}*/
+/*
+bool ready=false;
+
 void process(const anro1::mapMessage::ConstPtr& msg){
 	ROS_INFO("Ja slyszu");
 	if (msg->type == "fourLanes"){
@@ -227,14 +479,17 @@ void process(const anro1::mapMessage::ConstPtr& msg){
 		ready = true;
 	}
 }
+
+
+
 int main(int argc, char **argv)
 {
 
-	ros::init(argc, argv, "Lights");
+	ros::init(argc, argv, "Nodes");
 	srand(time(NULL));
 	ros::NodeHandle n;
-	ros::Publisher chatter_pub = n.advertise<anro1::lightsVector>("lights_info", 10);//utworzenie kanalu do nadawania
-	ros::Subscriber sub = n.subscribe("map_info", 20, process);//subskrypcja kanalu z informacjami
+	ros::Publisher chatter_pub = n.advertise<anro1::lightsVector>("lights_info", 1);//utworzenie kanalu do nadawania
+	ros::Subscriber sub = n.subscribe("map_info", 1, process);//subskrypcja kanalu z informacjami
 	ros::Rate loop_rate(10);
 	time_t lasttick, thistick;
 	time(&lasttick);
@@ -259,7 +514,7 @@ int main(int argc, char **argv)
 			lightmsg.y = (*it).y;
 			lightmsg.NS = (*it).northGreen;
 			lightmsg.WE = (*it).westGreen;
-			
+
 
 			lightVector.lights.push_back(lightmsg);
 			lightVector.size++;
@@ -274,4 +529,5 @@ int main(int argc, char **argv)
 
 	return 0;
 }
+
 */
