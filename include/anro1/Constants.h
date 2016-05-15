e7b923bd78be5eaedbf877e7090469b193dae4a6
Antoni Różański <ant.rozanski@gmail.com>
2016-05-15 16:22:32 -0700

    Smaller crossroads & .launch file added

--- include/anro1/Constants.h
+++ include/anro1/Constants.h
@@ -1,8 +1,8 @@
 #ifndef CONSTANTS_H
 #define CONSTANTS_H
 
-const double LANE_WIDTH = 0.123;
-const double MATRIX_DISTANCE = 0.5; //distance between map[i][j] nad map[i][j+1]
+const double LANE_WIDTH = 0.2;
+const double MATRIX_DISTANCE = 0.3; //distance between map[i][j] nad map[i][j+1]
 const double VIZ_LANE_WIDTH = 0.8* LANE_WIDTH;// width in rviz
 
 
